c870c9a1f785a17309cddc0042ec441636d69062
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-12-07 19:38:24 +1100

    Adds Bot id check
    stops death loop when a user send a message

--- CountingBot/countingbot.py
+++ CountingBot/countingbot.py
@@ -5,7 +5,10 @@ import discord
 client = discord.Client(intents=discord.Intents().all())
 
 BotToken = os.environ['BOTTOKEN']
-ChannelID = os.environ['CHANNELID']
+ChannelId = os.environ['CHANNELID']
+BotId1 = os.environ['BOTID1']
+BotId2 = os.environ['BOTID2']
+
 
 @client.event
 async def on_ready():
@@ -18,18 +21,34 @@ async def on_message(message):
     if message.author.id == client.user.id:
         return
 
-    if message.channel.id == int(ChannelID):
-        string = message.content
-        await sleep(1)
-        try:
-            counter = int(string)
-            counter += 1
-            await message.channel.send(counter)
-            print(counter)
-        except ValueError:
-            # Handle the exception
-            message.channel.send('Please enter an integer')
-            print('Please enter an integer')
+    if message.author.id == int(BotId1):
+        if message.channel.id == int(ChannelId):
+            string = message.content
+            await sleep(1)
+            try:
+                counter = int(string)
+                counter += 1
+                await message.channel.send(counter)
+                print(counter)
+            except ValueError:
+                # Handle the exception
+                message.channel.send('Please enter an integer')
+                print('Please enter an integer')
+    else:
+        if message.author.id == int(BotId2):
+            if message.channel.id == int(ChannelId):
+                string = message.content
+                await sleep(1)
+                try:
+                    counter = int(string)
+                    counter += 1
+                    await message.channel.send(counter)
+                    print(counter)
+                except ValueError:
+                    # Handle the exception
+                    message.channel.send('Please enter an integer')
+                    print('Please enter an integer')
 
 
 client.run(BotToken)
+
